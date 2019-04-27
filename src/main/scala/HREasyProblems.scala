f76cd49418cc3d742ab897d2d14bcb0020dec16e
xsobrietyx <vadym.serdiuk@gmail.com>
2019-04-27 17:24:11 +0300

    Tasks added, tests added.

--- src/main/scala/HREasyProblems.scala
+++ src/main/scala/HREasyProblems.scala
@@ -1,7 +1,7 @@
 /**
   * Created by xsobrietyx on 27-April-2019 time 15:44
   */
-object EasyProblems extends App {
+object HREasyProblems extends App {
   /*
   Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
   We define the rating for Alice's challenge to be the triplet a = (a[0],a[1],a[2]) , and the rating for Bob's challenge to be the triplet b = (b[0],b[1],b[2]).
@@ -20,5 +20,28 @@ object EasyProblems extends App {
       if (x._1 > x._2) res = (res._1 + 1, res._2) else if (x._2 > x._1) res = (res._1, res._2 + 1)
     })
     Array(res._1, res._2)
+  }
+
+  /*
+  Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+  For example, the square matrix is shown below:
+  1 2 3
+  4 5 6
+  9 8 9
+  For this particular case result should be: |(1 + 5 + 9) - (9 + 5 + 3)| = |15 - 17| = 2
+   */
+  def diagonalDifference(matrix: Array[Array[Int]]): Int = {
+    val i1: (Int, Int) = (0, 0)
+    val i2: (Int, Int) = (matrix.length - 1, 0)
+
+    def compute(iter0: Int = 0, iter1: (Int, Int), iter2: (Int, Int), result: (Int, Int) = (0, 0)): Int = {
+      if (iter0 == matrix.length) Math.abs(result._1 - result._2)
+      else compute(iter0 + 1,
+        (iter1._1 + 1, iter1._2 + 1),
+        (iter2._1 - 1, iter2._2 + 1),
+        (result._1 + matrix(iter1._1)(iter1._2), result._2 + matrix(iter2._1)(iter2._2)))
+    }
+
+    compute(iter1 = i1, iter2 = i2)
   }
 }
